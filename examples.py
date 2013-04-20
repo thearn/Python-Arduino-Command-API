1b8baa1024b6cc63e862912ca98209ff4ee54c74
Tristan Hearn <tristanhearn@gmail.com>
2013-04-19 23:54:21 -0400

    made the com port/device name set-able in the examples

--- examples.py
+++ examples.py
@@ -2,11 +2,11 @@
 from Arduino import Arduino
 import time
 
-def Blink(led_pin,baud):
+def Blink(led_pin,baud, port = ""):
     """
     Blinks an LED in 1 sec intervals
     """
-    board = Arduino(baud)
+    board = Arduino(baud, port=port)
     while True:
         board.digitalWrite(led_pin,"LOW")
         print board.digitalRead(led_pin) #confirm LOW (0)
@@ -15,12 +15,12 @@ def Blink(led_pin,baud):
         print board.digitalRead(led_pin) #confirm HIGH (1)
         time.sleep(1)
 
-def softBlink(led_pin,baud):
+def softBlink(led_pin,baud, port = ""):
     """
     Fades an LED off and on, using 
     Arduino's analogWrite (PWM) function
     """    
-    board=Arduino(baud)
+    board=Arduino(baud, port=port)
     i=0
     while True:
         i+=1
@@ -30,12 +30,12 @@ def softBlink(led_pin,baud):
                 k=510-k
             board.analogWrite(led_pin,k)
 
-def adjustBrightness(pot_pin,led_pin,baud):
+def adjustBrightness(pot_pin,led_pin,baud, port=""):
     """
     Adjusts brightness of an LED using a
     potentiometer
     """    
-    board=Arduino(baud)
+    board=Arduino(baud, port=port)
     while True:
         time.sleep(0.01)
         val=board.analogRead(pot_pin)/4
@@ -43,12 +43,12 @@ def adjustBrightness(pot_pin,led_pin,baud):
         board.analogWrite(led_pin,val)
 
 
-def PingSonar(pw_pin,baud):
+def PingSonar(pw_pin,baud, port=""):
     """
     Gets distance measurement from Ping))) 
     ultrasonic rangefinder connected to pw_pin
     """
-    board = Arduino(baud)
+    board = Arduino(baud, port=port)
     pingPin=pw_pin
     while True:
         duration = board.pulseIn(pingPin, "HIGH")
@@ -57,12 +57,12 @@ def PingSonar(pw_pin,baud):
         print inches,"inches"
         time.sleep(0.1)
         
-def LCD(tx,baud,ssbaud,message):
+def LCD(tx,baud,ssbaud,message, port=""):
     """
     Prints to two-line LCD connected to 
     pin tx
     """
-    board = Arduino(baud)
+    board = Arduino(baud, port=port)
     board.SoftwareSerial.begin(0,tx,ssbaud)
     while True:
         board.SoftwareSerial.write(" test ") 
