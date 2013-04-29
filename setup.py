7930226a19e491204660007883234d874de8183f
Tristan Hearn <tristanhearn@gmail.com>
2013-04-29 18:09:51 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='Python Arduino Command API',
     description="a light-weight Python library for communicating with Arduino microcontroller boards",
     author='Tristan Hearn',
     author_email='tristanhearn@gmail.com',
-    url='https://github.com/thearn/pickle-gzip',
+    url='https://github.com/thearn/Python-Arduino-Command-API',
     license='Apache 2.0',
     packages=['Arduino'],
 )
